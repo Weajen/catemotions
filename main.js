a833b1ef7ab6b5e090193460fc61b09418453432
Weajen <kristenandfer@gmail.com>
2023-01-22 23:07:13 +0100

    update js

--- main.js
+++ main.js
@@ -6,21 +6,24 @@ const button4 = document.querySelector("#button4");
 const button5 = document.querySelector("#button5");
 
 // déclaration constantes: images de chat
-const image = document.querySelector("#image")
-const img1 = document.querySelector("#image1")
-const img2 = document.querySelector("#image2")
-const img3 = document.querySelector("#image3")
-const img4 = document.querySelector("#image4")
-const img5 = document.querySelector("#image5")
+const img = document.querySelector("#image");
+const img1 = document.querySelector("#image1");
+const img2 = document.querySelector("#image2");
+const img3 = document.querySelector("#image3");
+const img4 = document.querySelector("#image4");
+const img5 = document.querySelector("#image5");
 
 let intervalParty;
 
 // chat qui danse
 button1.addEventListener("click", () => {
-  image,img2,img3,img4,img5.style.display = "none";
+  img.style.visibility= "hidden";
+  img2.style.visibility= "hidden"
+  img3.style.visibility= "hidden"
+  img4.style.visibility= "hidden"
+  img5.style.visibility= "hidden"
   img1.style.visibility = "visible";
-  document.getElementById("texte").innerHTML = 
-  "Le chat fait la teuf!!!";
+  document.getElementById("texte").innerHTML = "Le chat fait la teuf!!!";
   intervalParty = setInterval(() => {
     document.body.style.backgroundColor = `rgb(
         ${Math.floor(Math.random() * 255)}
@@ -31,41 +34,52 @@ button1.addEventListener("click", () => {
 });
 // chat triste
 button2.addEventListener("click", () => {
-  image,img1,img3,img4,img5.style.display = "none";
+  img.style.visibility= "hidden";
+  img1.style.visibility= "hidden"
+  img3.style.visibility= "hidden"
+  img4.style.visibility= "hidden"
+  img5.style.visibility= "hidden"
   img2.style.visibility = "visible";
   clearInterval(intervalParty);
-  document.getElementById("texte").innerHTML = 
-  "Le chat est tout tristou :(";
+  document.getElementById("texte").innerHTML = "Le chat est tout tristou :(";
   document.body.style.backgroundColor = `rgb(75, 97, 179)`;
 });
 
 // chat content
 button3.addEventListener("click", () => {
-  image,img1,img2,img4,img5.style.display = "none";
+  img.style.visibility= "hidden";
+  img1.style.visibility= "hidden"
+  img2.style.visibility= "hidden"
+  img4.style.visibility= "hidden"
+  img5.style.visibility= "hidden"
   img3.style.visibility = "visible";
   clearInterval(intervalParty);
-  document.getElementById("texte").innerHTML = 
-  "Le chat est trop content!!";
+  document.getElementById("texte").innerHTML = "Le chat est trop content!!";
   document.body.style.backgroundColor = `rgb(250, 230, 140)`;
 });
 
 // chat faché
 button4.addEventListener("click", () => {
-  image,img1,img2,img3,img5.style.display = "none";
+  img.style.visibility= "hidden";
+  img1.style.visibility= "hidden"
+  img2.style.visibility= "hidden"
+  img3.style.visibility= "hidden"
+  img5.style.visibility= "hidden"
   img4.style.visibility = "visible";
   clearInterval(intervalParty);
-  document.getElementById("texte").innerHTML =
-    "Le chat est pas content du tout >:( il fait la baston!!";
+  document.getElementById("texte").innerHTML = "Le chat est pas content du tout >:( il fait la baston!!";
   document.body.style.backgroundColor = `rgb(224, 56, 47)`;
 });
 
 // chat fatigué
 button5.addEventListener("click", () => {
-  image,img1,img2,img3,img4.style.display = "none";
+  img.style.visibility= "hidden";
+  img1.style.visibility= "hidden"
+  img2.style.visibility= "hidden"
+  img3.style.visibility= "hidden"
+  img4.style.visibility= "hidden"
   img5.style.visibility = "visible";
   clearInterval(intervalParty);
-  document.getElementById("texte").innerHTML =
-    "Le chat est fatigué. On le laisse faire dodo :)";
+  document.getElementById("texte").innerHTML = "Le chat est fatigué. On le laisse faire dodo :)";
   document.body.style.backgroundColor = `rgb(84, 40, 150)`;
 });
-
